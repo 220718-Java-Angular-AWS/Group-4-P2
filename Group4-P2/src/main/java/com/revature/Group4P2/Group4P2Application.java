096d4ba288f791b41047a221afb78b6360e19d67
Rebecca Candelaria <candelaria.rebecca.d@gmail.com>
2022-08-30 16:42:18 -0400

    Uploading Spring Boot with required dependencies

---
+++ Group4-P2/src/main/java/com/revature/Group4P2/Group4P2Application.java
@@ -0,0 +1,13 @@
+package com.revature.Group4P2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4P2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4P2Application.class, args);
+	}
+
+}
